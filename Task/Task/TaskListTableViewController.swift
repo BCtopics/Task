56fa971eaa06ebe68428ac9d2dde5f865b16f5b2
Bradley Gilmore <bradleygilmoreefuf@yahoo.com>
2017-05-31 15:22:51 -0600

    Implement Segues/Pass Information

--- Task/Task/TaskListTableViewController.swift
+++ Task/Task/TaskListTableViewController.swift
@@ -48,30 +48,33 @@ class TaskListTableViewController: UITableViewController {
             
         }    
     }
-    
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
 
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if segue.identifier == "detailSegue" {
+            if let vc = segue.destination as? TaskDetailTableViewController {
+                let index = self.tableView.indexPathForSelectedRow?.row
+                
+                let task = TaskController.shared.tasks[index!] // Fix bang
+                vc.task = task
+                guard let due = task.due as? Date else { return }
+                vc.dueDateValue = due
+            }
+        }
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
     }
-    */
+    
 
 }
+
+
+
+
+
+
+
+

1d7627e1cc19d527e2cde1928e3fc8eec04e9e06
Bradley Gilmore <bradleygilmoreefuf@yahoo.com>
2017-05-31 14:47:15 -0600

    Implement Swipe to delete

--- Task/Task/TaskListTableViewController.swift
+++ Task/Task/TaskListTableViewController.swift
@@ -37,28 +37,18 @@ class TaskListTableViewController: UITableViewController {
 
         return cell
     }
-    
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
+            let tc = TaskController.shared
+            let task = tc.tasks[indexPath.row]
+            tc.remove(task: task)
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+            
         }    
     }
-    */
-
+    
     /*
     // Override to support rearranging the table view.
     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {

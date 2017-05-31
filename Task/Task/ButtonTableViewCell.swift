0755352755bdd14f6dbbda964afddb13511b8a66
Bradley Gilmore <bradleygilmoreefuf@yahoo.com>
2017-05-31 15:55:13 -0600

    Create custom cell

---
+++ Task/Task/ButtonTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  ButtonTableViewCell.swift
+//  Task
+//
+//  Created by Bradley GIlmore on 5/31/17.
+//  Copyright © 2017 Bradley Gilmore. All rights reserved.
+//
+
+import UIKit
+
+class ButtonTableViewCell: UITableViewCell {
+    
+    //MARK: - IBOutlets
+    
+    @IBOutlet weak var primaryLabel: UILabel!
+    @IBOutlet weak var completeButton: UIButton!
+    
+    //MARK: - IBActions
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        
+        
+        
+    }
+    
+    
+}
--- Task/Task/TaskListTableViewController.swift
+++ Task/Task/TaskListTableViewController.swift
@@ -30,7 +30,7 @@ class TaskListTableViewController: UITableViewController {
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "TaskCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "taskCell", for: indexPath)
 
         let task = TaskController.shared.tasks[indexPath.row]
         cell.textLabel?.text = task.name

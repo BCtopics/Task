81399a95525d94c116b0fc15a0c4e9808d268233
Bradley Gilmore <bradleygilmoreefuf@yahoo.com>
2017-05-31 14:08:48 -0600

    Implement CoreDataModel, and CoreDataStack

---
+++ Task/Task/Task+Convenience.swift
@@ -0,0 +1,22 @@
+//
+//  Task+Convenience.swift
+//  Task
+//
+//  Created by Bradley GIlmore on 5/31/17.
+//  Copyright © 2017 Bradley Gilmore. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+extension Task {
+    convenience init(name: String, notes: String? = nil, due: Date? = nil, context: NSManagedObjectContext = CoreDataStack.context) {
+        
+        self.init(context: context)
+        
+        self.name = name
+        self.notes = notes
+        self.due = due as NSDate?
+        self.isComplete = false
+    }
+}

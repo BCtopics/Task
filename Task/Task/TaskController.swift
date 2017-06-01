811445b818f52401f26e705370ac724d865924cc
Bradley Gilmore <bradleygilmoreefuf@yahoo.com>
2017-06-01 09:26:02 -0600

    Save/Fetch requests. Segue bugfix

--- Task/Task/TaskController.swift
+++ Task/Task/TaskController.swift
@@ -35,26 +35,50 @@ class TaskController {
     
     func add(taskWithName name: String, notes: String?, due: Date?) {
         
+        let _ = Task(name: name, notes: notes, due: due)
+        saveToPersistentStore()
+        tasks = fetchTasks()
+        
     }
     
     func update(task: Task, name: String, notes: String?, due: Date?) {
+        
+        task.name = name
+        task.notes = notes
+        task.due = due as NSDate?
+        saveToPersistentStore()
+        tasks = fetchTasks()
         
     }
     
     func remove(task: Task) {
         
-    }
-    
-    func saveToPersistentStore() {
+        task.managedObjectContext?.delete(task)
+        saveToPersistentStore()
+        tasks = fetchTasks()
         
-    }
-    
-    func fetchTasks() -> [Task] {
-        return mockTasks
     }
     
     func toggleIsCompleteFor(task: Task) {
         task.isComplete = !task.isComplete
+        saveToPersistentStore()
+    }
+    
+    
+    // MARK: Persistence
+    
+    private func saveToPersistentStore() {
+        
+        do {
+            try CoreDataStack.context.save()
+        } catch {
+            print("Error saving Managed Object Context. Items not saved.")
+        }
+    }
+    
+    private func fetchTasks() -> [Task] {
+        let request: NSFetchRequest<Task> = Task.fetchRequest()
+        return (try? CoreDataStack.context.fetch(request)) ?? []
     }
     
 }
